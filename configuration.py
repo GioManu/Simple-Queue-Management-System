0ce875f02e305c96ded318662348435109db3374
Gio Manu <giomanu.gtr@gmail.com>
2019-09-25 17:21:43 +0400

    correction

--- configuration.py
+++ configuration.py
@@ -4,10 +4,6 @@ class Configuration:
 
         CHECKS_PATH = "checks.txt"
 
-        CHECK_FILE = "checkInfo.json"   # File to write in
-        CHECK_ADDRS = ["C:\printInfo\printer2.ini"] # list of Files to read from
-        CHECK_MANUF = ["სამზარეულო","ბარი"] # list of Files to read from
-
         STATE_IN_PROGRESS = 0
         STATE_DONE = 1
         STATE_CLOSED = 2
