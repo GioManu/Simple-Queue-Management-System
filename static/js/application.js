7dddb6bfecd555cd7fccab80e40771e2146495e8
Gio Manu <giomanu.gtr@gmail.com>
2019-09-26 15:57:42 +0400

    front changes

--- static/js/application.js
+++ static/js/application.js
@@ -1,33 +1,30 @@
+$(document).ready(function() {
+  //connect to the socket server.
+  var socket = io.connect(
+    "http://" + document.domain + ":" + location.port + "/QueueMonitor"
+  );
+  var numbers_received = [];
 
-$(document).ready(function(){
-    //connect to the socket server.
-    var socket = io.connect('http://' + document.domain + ':' + location.port + '/QueueMonitor');
-    var numbers_received = [];
+  //receive details from server
+  socket.on("Result", function(msg) {
+    //maintain a list of ten numbers
+    if (numbers_received.length >= 10) {
+      numbers_received.shift();
+    }
 
-    //receive details from server
-    socket.on('Result', function(msg) {
-        console.log(JSON.stringify(msg));
-        //maintain a list of ten numbers
-        if (numbers_received.length >= 10){
-            numbers_received.shift()
-        }
-        console.log(msg.objects);
+    inPogress = "";
+    Done = "";
 
-        inPogress = '';
-        Done = '';
+    for (let [key, value] of Object.entries(msg.objects)) {
+      if (value == "0") {
+        inPogress =
+          inPogress + '<div class="check_nums" ><p>' + key + "</p></div>";
+      } else {
+        Done = Done + '<div class="check_nums"><p>' + key + "</p></div>";
+      }
+    }
 
-        for (let [key, value] of Object.entries(msg.objects)) {
-
-            if(value == "0"){
-                inPogress  = inPogress + '<div class="check_nums" ><p>' + key + '</p></div>';
-            }else{
-                Done = Done + '<div class="check_nums"><p>' + key + '</p></div>';
-            }
-          console.log(`${key}: ${value}`);
-        }
-
-        $('#InProgContainer').html(inPogress);
-        $('#DoneContainer').html(Done);
-    });
-
+    $("#InProgContainer").html(inPogress);
+    $("#DoneContainer").html(Done);
+  });
 });
---
+++ static/js/dist.js
@@ -0,0 +1,34 @@
+
+let closeBtn = document.getElementById("CloseCheckButton");
+
+let lis = document.getElementById("DoneContainer");
+
+lis.addEventListener("click", e => {
+if(e.target.parentElement.classList.contains("check_nums")){
+    e.target.parentElement.classList.toggle("selected");
+}
+});
+
+closeBtn.addEventListener("click",e =>{
+    let elems = document.getElementsByClassName("selected");
+    data = []
+
+    for(let i=0;i<elems.length;i++){
+        data.push(elems[i].firstChild.innerText);
+    }
+    if(data.length > 0 ){
+        closeChecksRequest(data,2);
+    }
+});
+
+async function closeChecksRequest(data,mode){
+    let url = `/registerNum/${mode}`
+    console.log(url);
+    let response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(data)
+    });
+}
